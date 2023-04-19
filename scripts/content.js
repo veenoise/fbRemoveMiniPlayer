75df127d11ba95c1dc814e4956dc4c5a80a8cbca
veenoise <68385179+veenoise@users.noreply.github.com>
2023-04-19 14:08:29 +0800

    Changed the script because DOMSubtreeModified is deprecated
    Used MutationObserver instead

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,11 @@
-(function(document,found) {
-    window.addEventListener('DOMSubtreeModified', function() {
-  
-      var yourdiv = document.querySelector("div.xxsgkw5");
+const mutationObserver = new MutationObserver(entries => {  
+  const targetDiv = document.querySelector("div.xal61yo.x1jn9clo.x2c8kfy.x78zum5.x164qtfw.xl56j7k.xz65tgg.xixxii4.xxsgkw5")
+  if (targetDiv) {
+    parent.removeChild(targetDiv)
+  }
+})
 
-      if(yourdiv){
-        yourdiv.parentElement.removeChild(yourdiv);
-        found = true;
-      }
-    }, false);
-})(document,false);
+const parent =  document.querySelector("div.x9f619.x1n2onr6.x1ja2u2z")
+
+mutationObserver.observe(parent, { childList: true })
+
