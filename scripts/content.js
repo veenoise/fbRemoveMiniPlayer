49dcaa036327fded7709cb017b153f1046841d11
veenoise <68385179+veenoise@users.noreply.github.com>
2023-05-02 16:13:22 +0800

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,12 @@
-const mutationObserver = new MutationObserver(entries => {  
-  const targetDiv = document.querySelector(".xxsgkw5")
-  if (targetDiv) {    
-    parent.removeChild(targetDiv)
-  }
-})
-
-const parent =  document.querySelector("div.x9f619.x1n2onr6.x1ja2u2z")
-
-mutationObserver.observe(parent, { childList: true })
-
+setTimeout(function() {
+  const mutationObserver = new MutationObserver(entries => {  
+    const targetDiv = document.querySelector(".xxsgkw5")
+    if (targetDiv) {    
+      parent.removeChild(targetDiv)
+    }
+  })
+  
+  const parent =  document.querySelector("div.x9f619.x1n2onr6.x1ja2u2z")
+  
+  mutationObserver.observe(parent, { childList: true })
+}, 5000);
